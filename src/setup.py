d74ca50fe8806d9cbf31de288e90ff94f311d405
Zen <z@pyl.onl>
2023-11-23 10:41:17 -0600

    added setup.py
    Signed-off-by: Zen <z@pyl.onl>

---
+++ src/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+from zenlib import __version__ as version
+
+
+setup(
+    name="zenlib",
+    version=version,
+    description="Useful python decorators and functions",
+    author="desultory"
+)

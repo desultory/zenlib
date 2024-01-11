c322645d0f84912d53dc4f67231795c380d4c6f6
Zen <z@pyl.onl>
2024-01-11 13:53:59 -0600

    remove clear_widgets as it makes qt a dep
    Signed-off-by: Zen <z@pyl.onl>

--- src/zenlib/util/clear_widgets.py
+++
@@ -1,9 +0,0 @@
-from PyQt6.QtWidgets import QWidgetItem
-
-
-# This function is used to clear the widgets from the layout
-def clear_widgets(qt_object):
-    for i in range(qt_object.count()):
-        item = qt_object.itemAt(i)
-        if isinstance(item, QWidgetItem) and item.widget():
-            item.widget().deleteLater()

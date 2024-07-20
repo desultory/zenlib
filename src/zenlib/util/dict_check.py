07df9d07207a5a8af9b34d42cb69097238c6c1df
Zen <z@pyl.onl>
2024-07-20 17:20:35 -0500

    clean imports, expand dict_check to check for unset keys
    Signed-off-by: Zen <z@pyl.onl>

--- src/zenlib/util/dict_check.py
+++ src/zenlib/util/dict_check.py
@@ -1,5 +1,7 @@
 __author__ = "desultory"
-__version__ = "1.1.0"
+__version__ = "1.1.1"
+
+from functools import wraps
 
 
 def return_check(self, message, raise_exception, log_level, return_val=None):
@@ -10,17 +12,39 @@ def return_check(self, message, raise_exception, log_level, return_val=None):
 
 
 def contains(key, message=None, is_set=True, raise_exception=False, log_level=10, debug_level=5):
+    """
+    Ensure that the key exists in the dictionary.
+    Returns the message/exception if the key is not found.
+    If is_set is True, then the key must have a value.
+    """
     def _dict_contains(func):
-        from functools import wraps
-
         @wraps(func)
         def _contains(*args, **kwargs):
             self = args[0]
+            msg = f"[{func.__name__}] {message}" if message else None
             if key not in self:
-                return return_check(self, message or "[%s] Unable to find key: %s." % (self.__class__.__name__, key), raise_exception, log_level)
-            if is_set and not self[key]:
-                return return_check(self, message or "[%s] Key is not set: %s." % (self.__class__.__name__, key), raise_exception, log_level)
-            self.logger.log(debug_level, "[%s] Contains check passed for: %s" % (self.__class__.__name__, key))
+                return return_check(self, msg or "[%s] Unable to find key: %s." % (func.__name__, key), raise_exception, log_level)
+            if is_set and not (self[key] or repr(key) == "PosixPath('.')"):
+                return return_check(self, msg or "[%s] Key is not set: %s." % (func.__name__, key), raise_exception, log_level)
+            self.logger.log(debug_level, "[%s] Contains check passed for: %s" % (func.__name__, key))
             return func(*args, **kwargs)
         return _contains
     return _dict_contains
+
+
+def unset(key, message=None, raise_exception=False, log_level=10, debug_level=5):
+    """
+    Ensure that the key does not exist in the dictionary.
+    If it exists, make sure it is not set.
+    """
+    def _dict_unset(func):
+        @wraps(func)
+        def _unset(*args, **kwargs):
+            self = args[0]
+            msg = f"[{func.__name__}] {message}" if message else None
+            if key in self and (self[key] or repr(key) == "PosixPath('.')"):
+                return return_check(self, msg or "[%s] Key is set: %s." % (func.__name__, key), raise_exception, log_level)
+            self.logger.log(debug_level, "[%s] Unset check passed for: %s" % (func.__name__, key))
+            return func(*args, **kwargs)
+        return _unset
+    return _dict_unset

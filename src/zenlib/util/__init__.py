b0fcf7e56f785dbb889f46254d0a40117f7b3033
Zen <z@pyl.onl>
2024-07-20 15:56:26 -0500

    make contains decorator for dict
    Signed-off-by: Zen <z@pyl.onl>

--- src/zenlib/util/__init__.py
+++ src/zenlib/util/__init__.py
@@ -5,7 +5,7 @@ from .replace_file_line import replace_file_line
 from .update_init import update_init
 from .walk_dict import walk_dict
 from .check_dict import check_dict
-from .dict_check import DictCheck
+from .dict_check import contains
 from .main_funcs import init_logger
 from .main_funcs import process_args
 from .main_funcs import init_argparser
@@ -16,4 +16,4 @@ from .main_funcs import get_kwargs
 __all__ = ['handle_plural', 'NoDupFlatList', 'pretty_print', 'replace_file_line',
            'update_init', 'walk_dict', 'check_dict', 'init_logger', 'process_args',
            'init_argparser', 'get_args_n_logger', 'get_kwargs_from_args', 'get_kwargs',
-           'DictCheck']
+           'contains']
--- src/zenlib/util/dict_check.py
+++ src/zenlib/util/dict_check.py
@@ -1,28 +1,26 @@
 __author__ = "desultory"
-__version__ = "1.0.0"
+__version__ = "1.1.0"
 
 
-class DictCheck:
-    """
-    Expects the class has self.logger and is a dict.
-    Mixin class for checking dict keys.
-    """
+def return_check(self, message, raise_exception, log_level, return_val=None):
+    if raise_exception:
+        raise ValueError(message)
+    self.logger.log(log_level, message)
+    return return_val
 
-    def _return_check(self, message, raise_exception, log_level, return_val=None):
-        if raise_exception:
-            raise ValueError(message)
-        self.logger.log(log_level, message)
-        return return_val
 
-    def _dict_contains(self, key, message=None, is_set=True, raise_exception=False, log_level=10, debug_level=5):
-        """
-        Checks if the dict contains a key.
-        if is_set is true, checks that the key is to something other than None.
-        """
-        self.logger.log(debug_level, "[%s] Checking dict key: %s" % (self.__class__.__name__, key))
-        if key not in self:
-            return self._return_check(message or "[%s] Unable to find key: %s." % (self.__class__.__name__, key), raise_exception, log_level)
+def contains(key, message=None, is_set=True, raise_exception=False, log_level=10, debug_level=5):
+    def _dict_contains(func):
+        from functools import wraps
 
-        if is_set and not self[key]:
-            return self._return_check(message or "[%s] Key is not set: %s." % (self.__class__.__name__, key), raise_exception, log_level)
-
+        @wraps(func)
+        def _contains(*args, **kwargs):
+            self = args[0]
+            if key not in self:
+                return return_check(self, message or "[%s] Unable to find key: %s." % (self.__class__.__name__, key), raise_exception, log_level)
+            if is_set and not self[key]:
+                return return_check(self, message or "[%s] Key is not set: %s." % (self.__class__.__name__, key), raise_exception, log_level)
+            self.logger.log(debug_level, "[%s] Contains check passed for: %s" % (self.__class__.__name__, key))
+            return func(*args, **kwargs)
+        return _contains
+    return _dict_contains

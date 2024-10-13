ac128a033d399c064c33697e21aa290521be1653
Zen <z@pyl.onl>
2024-10-13 13:08:07 -0500

    black format main __init__
    Signed-off-by: Zen <z@pyl.onl>

--- src/zenlib/__init__.py
+++ src/zenlib/__init__.py
@@ -1,7 +1,14 @@
 from .logging import ColorLognameFormatter, loggify
-from .util import handle_plural, NoDupFlatList, pretty_print, replace_file_line, update_init, walk_dict, check_dict
+from .util import NoDupFlatList, check_dict, handle_plural, pretty_print, replace_file_line, update_init, walk_dict
 
-__all__ = ['ColorLognameFormatter', 'loggify', 'handle_plural', 'NoDupFlatList', 'pretty_print', 'replace_file_line',
-           'update_init', 'walk_dict', 'check_dict']
-
-
+__all__ = [
+    "ColorLognameFormatter",
+    "loggify",
+    "handle_plural",
+    "NoDupFlatList",
+    "pretty_print",
+    "replace_file_line",
+    "update_init",
+    "walk_dict",
+    "check_dict",
+]

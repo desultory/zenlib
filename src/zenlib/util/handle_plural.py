3e247af253e416a89f6f990f7e05981557e1f31f
Zen <z@pyl.onl>
2024-10-13 12:36:08 -0500

    update handle_plural to work without a logger, add tests
    Signed-off-by: Zen <z@pyl.onl>

--- src/zenlib/util/handle_plural.py
+++ src/zenlib/util/handle_plural.py
@@ -1,15 +1,20 @@
 __author__ = "desultory"
-__version__ = "2.0.0"
+__version__ = "2.1.0"
 
 from collections.abc import KeysView, ValuesView
 
 
-def handle_plural(function):
+def handle_plural(function, log_level=10):
     """
-    Wraps functions to take a list/dict and iterate over it
-    the last argument should be iterable
+    Wraps functions to take a list/dict and iterate over it.
+    The last passed argument should be the iterable.
+    Logs using the logger attribute if it exists.
     """
     def wrapper(self, *args):
+        def log(msg, level=log_level):
+            if hasattr(self, "logger"):
+                self.logger.log(level, msg)
+
         if len(args) == 1:
             focus_arg = args[0]
             other_args = tuple()
@@ -18,22 +23,22 @@ def handle_plural(function):
             other_args = args[:-1]
 
         if isinstance(focus_arg, list) and not isinstance(focus_arg, str):
-            self.logger.debug("Expanding list: %s" % focus_arg)
+            log("Expanding list: %s" % focus_arg)
             for item in focus_arg:
                 function(self, *(other_args + (item,)))
         elif isinstance(focus_arg, ValuesView):
-            self.logger.debug("Expanding dict values: %s" % focus_arg)
+            log("Expanding dict values: %s" % focus_arg)
             for value in focus_arg:
                 function(self, *(other_args + (value,)))
         elif isinstance(focus_arg, KeysView):
-            self.logger.debug("Expanding dict keys: %s" % focus_arg)
+            log("Expanding dict keys: %s" % focus_arg)
             for key in focus_arg:
                 function(self, *(other_args + (key,)))
         elif isinstance(focus_arg, dict):
-            self.logger.debug("Expanding dict: %s" % focus_arg)
+            log("Expanding dict: %s" % focus_arg)
             for key, value in focus_arg.items():
                 function(self, *(other_args + (key, value,)))
         else:
-            self.logger.log(5, "Arguments were not expanded: %s" % args)
+            log(f"Arguments were not expanded: {args}", log_level - 5)
             return function(self, *args)
     return wrapper
---
+++ tests/test_handle_plural.py
@@ -0,0 +1,51 @@
+from unittest import TestCase, main
+
+from zenlib.util import handle_plural
+
+
+class TestHandlePlural(TestCase):
+    @handle_plural
+    def _test_plural_ints(self, arg_a, iterated):
+        if isinstance(iterated, int):
+            self._test_data += iterated
+        return iterated
+
+    def test_list(self):
+        self._test_data = 0
+        test_list = [1, 2, 3, 4]
+        extra_arg = 'a'
+        self.assertEqual(self._test_plural_ints(extra_arg, test_list), None)
+        self.assertEqual(self._test_data, sum(test_list))
+
+    def test_single(self):  # non-iterables should allow returns
+        self._test_data = 0
+        test_arg = 1
+        extra_arg = 'a'
+        self.assertEqual(self._test_plural_ints(extra_arg, test_arg), test_arg)
+
+    def test_string(self):
+        self._test_data = 0
+        test_arg = 'abcd'
+        extra_arg = 'z'
+        self.assertEqual(self._test_plural_ints(extra_arg, test_arg), test_arg)
+
+    def test_dict_keys(self):
+        self._test_data = 0
+        test_arg = {'a': 1, 'b': 2, 'c': 3}
+        self.assertEqual(self._test_plural_ints(test_arg), None)
+        self.assertEqual(self._test_data, sum(test_arg.values()))
+
+    @handle_plural
+    def _test_plural_dict(self, key, value):
+        self._test_data[key] = value
+
+    def test_dict_unrolling(self):
+        self._test_data = {}
+        test_arg = {'a': 1, 'b': 2, 'c': 3}
+        self._test_plural_dict(test_arg)
+        self.assertEqual(self._test_data, test_arg)
+
+
+if __name__ == '__main__':
+    main()
+

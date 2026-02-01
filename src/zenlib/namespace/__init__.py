995921676c9d8fe91d674cc4a5dda6feb606b452
Zen <z@pyl.onl>
2026-02-01 16:49:42 -0600

    improve typing for nsexec, get_id_map
    may not be importable, may be
    
    Signed-off-by: Zen <z@pyl.onl>

--- src/zenlib/namespace/__init__.py
+++ src/zenlib/namespace/__init__.py
@@ -1,11 +1,16 @@
 from platform import system
 from sys import version_info
+from typing import Callable, Union
+
+nsexec: Union[Callable, None]
+get_id_map: Union[Callable, None]
+
 
 if version_info < (3, 12) or system() != "Linux":
     nsexec, get_id_map = None, None
 else:
-    from zenlib.namespace.namespace import get_id_map  # type: ignore[assignment]
-    from zenlib.namespace.nsexec import nsexec  # type: ignore[assignment]
+    from zenlib.namespace.namespace import get_id_map
+    from zenlib.namespace.nsexec import nsexec
 
 __all__ = [
     "nsexec",

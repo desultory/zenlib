1f0738a6cd46865f4a229cad13416b8bde1ffeb1
Zen <z@pyl.onl>
2026-02-01 14:17:03 -0600

    ignore type assignements for dynamic imports
    Signed-off-by: Zen <z@pyl.onl>

--- src/zenlib/namespace/__init__.py
+++ src/zenlib/namespace/__init__.py
@@ -4,8 +4,8 @@ from sys import version_info
 if version_info < (3, 12) or system() != "Linux":
     nsexec, get_id_map = None, None
 else:
-    from zenlib.namespace.namespace import get_id_map
-    from zenlib.namespace.nsexec import nsexec
+    from zenlib.namespace.namespace import get_id_map  # type: ignore[assignment]
+    from zenlib.namespace.nsexec import nsexec  # type: ignore[assignment]
 
 __all__ = [
     "nsexec",

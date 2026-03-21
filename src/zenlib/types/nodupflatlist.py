1b45fba9cc5d6fb15f4581f35ab2995eefb0b0bd
Zen <z@pyl.onl>
2026-03-21 13:56:10 -0500

    make nodupflatlist accept an element type
    Signed-off-by: Zen <z@pyl.onl>

--- src/zenlib/types/nodupflatlist.py
+++ src/zenlib/types/nodupflatlist.py
@@ -1,22 +1,25 @@
 __author__ = "desultory"
-__version__ = "1.2.0"
+__version__ = "1.3.0"
 
 from collections.abc import Iterable
-from typing import Any, Self
+from typing import Self, TypeVar
 
 from zenlib.logging.classlogger import ClassLogger
 from zenlib.util.handle_plural import handle_plural
 
+T = TypeVar("T")
 
-class NoDupFlatList(ClassLogger, list):
+
+class NoDupFlatList(ClassLogger, list[T]):
     """List that automatically filters duplicate elements when appended and concatenated."""
+
     def __init__(self, no_warn=False, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.no_warn = no_warn
 
     @handle_plural
-    def append(self, item):
-        """ Adds an item or items to the list, avoiding duplicates.
+    def append(self, item: T | Iterable[T]) -> None:
+        """Adds an item or items to the list, avoiding duplicates.
         If the item is an iterable (but not a string), each element is added individually. (recursive)
         """
         if isinstance(item, Iterable) and not isinstance(item, str):
@@ -29,7 +32,7 @@ class NoDupFlatList(ClassLogger, list):
             self.logger.warning("List item already exists: %s" % item)
 
     # Ignore type checking for __iadd__ method, we think we know what we're doing
-    def __iadd__(self, item: Iterable[Any]) -> Self:  # type: ignore[misc]
+    def __iadd__(self, item: Iterable[T]) -> Self:  # type: ignore[misc,override]
         """Implements the += operator to add items from an iterable, avoiding duplicates.
         Passes any input to the append method.
 
